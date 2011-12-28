a9002ee943786720ce45bce6e388baa624cce088
Tac Tacelosky <tacman@gmail.com>
2011-12-28 16:50:45 -0500

    read longer line, refactor to allow for tab delimiter

--- lib/EasyCSV/Reader.php
+++ lib/EasyCSV/Reader.php
@@ -10,15 +10,19 @@ class Reader extends AbstractBase
     public function __construct($path, $mode = 'r+')
     {
         parent::__construct($path, $mode);
-        $this->_headers = $this->getRow();
         $this->_line    = 0;
     }
 
     public function getRow()
     {
-        if (($row = fgetcsv($this->_handle, 1000, $this->_delimiter, $this->_enclosure)) !== false) {
+        if (($row = fgetcsv($this->_handle, 4096, $this->_delimiter, $this->_enclosure)) !== false) {
             $this->_line++;
-            return $this->_headers ? array_combine($this->_headers, $row) : $row;
+            if (empty($this->_headers)) {
+              $this->_headers = $row;
+              return $this->getRow();
+            } else {
+              return array_combine($this->_headers, $row);
+            }
         } else {
             return false;
         }
