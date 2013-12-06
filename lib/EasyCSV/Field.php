37331960aa8b76f59f1f9832fd567fbe5035b2e2
Tac Tacelosky <tacman@gmail.com>
2013-12-06 15:55:59 -0500

    get unique values for choice questions

--- lib/EasyCSV/Field.php
+++ lib/EasyCSV/Field.php
@@ -13,6 +13,8 @@ class Field {
 	var $type;
     var $idx = 0;
     var $column_name; // original name from CSV
+    var $uniqueValues = [];
+    var $choiceCandidate = true;
 
 	function __construct($name, $column_name, $idx, $type='') {
 	  $this->column_name = $column_name;
@@ -31,11 +33,11 @@ class Field {
 	}
 
 	function check($value) {
-		# printf("<li>Checking %s (%s)", $this->field_name, $value);
-		if ($value=='') {
-		  $this->is_unique = false;
-			return;
-    }
+        # printf("<li>Checking %s (%s)", $this->field_name, $value);
+        if ($value=='') {
+            $this->is_unique = false;
+            return;
+        }
 
 	  // if it's the first row, then set defaults
 	  if (!isset($this->max_length)) {
@@ -57,6 +59,13 @@ class Field {
   	if (strlen($value) < $this->min_length) {
 				$this->min_length = strlen($value);
 		}
+
+        // ugh, introduces a dependency we don't really need!
+        if (count($this->uniqueValues) < 32) {
+            $this->uniqueValues[\Survos\Lib\tt::name_to_code($value)] = $value;
+        } else {
+            $this->choiceCandidate = false;
+        }
 
 		if ($this->is_unique) {
 
